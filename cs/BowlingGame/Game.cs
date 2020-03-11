0003d611551aae08f64236d81848cfcc6de6bfc5
Ar-Chr <artyom.chr@mail.ru>
2020-03-11 12:24:08 +0300

    Double Strike Bonus added

--- cs/BowlingGame/Game.cs
+++ cs/BowlingGame/Game.cs
@@ -29,9 +29,14 @@ namespace BowlingGame
             {
                 currentFrame.FirstThrow = pins;
 
-                if (currentFrameNumber > 0 && (frames[currentFrameNumber - 1].State == FrameState.Strike ||
-                                               frames[currentFrameNumber - 1].State == FrameState.Spare))
+                if (currentFrameNumber > 0 && (frames[currentFrameNumber - 1].State == FrameState.Spare ||
+                                               frames[currentFrameNumber - 1].State == FrameState.Strike))
                 {
+                    if (frames[currentFrameNumber - 1].State == FrameState.Strike)
+                        if (currentFrameNumber > 1 && frames[currentFrameNumber - 2].State == FrameState.Strike)
+                        {
+                            frames[currentFrameNumber - 2].Bonus += pins;
+                        }
                     frames[currentFrameNumber - 1].Bonus += pins;
                 }
 
@@ -39,6 +44,9 @@ namespace BowlingGame
             }
             else
             {
+                if (pins + currentFrame.FirstThrow > 10)
+                    throw new ArgumentException();
+
                 currentFrame.SecondThrow = pins;
 
                 if (currentFrameNumber > 0 && frames[currentFrameNumber - 1].State == FrameState.Strike)
@@ -132,6 +140,86 @@ namespace BowlingGame
             game.Roll(8);
             game.Roll(3);
             game.GetScore().Should().Be(16);
+        }
+
+        [Test]
+        public void GetScore_AddsBonusScore_WhenStrike()
+        {
+            game.Roll(10);
+            game.Roll(8);
+            game.GetScore().Should().Be(26);
+        }
+
+        [Test]
+        public void GetScore_AddsBonusScoreTwice_WhenStrike()
+        {
+            game.Roll(10);
+            game.Roll(2);
+            game.Roll(4);
+            game.GetScore().Should().Be(22);
+        }
+
+        [Test]
+        public void GetScore_AddsBonusScore_WhenTwoStrikes()
+        {
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(4);
+            game.GetScore().Should().Be(42);
+        }
+
+        [Test]
+        public void Roll_DoesntAcceptRollsMoreThanPintsLeft()
+        {
+            Action action = () =>
+            {
+                game.Roll(5);
+                game.Roll(8);
+            };
+           action.ShouldThrow<ArgumentException>();
+        }
+
+        [Test]
+        public void GetScore_AddsBonusScore_WhenThreeStrikes()
+        {
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(2);
+            game.GetScore().Should().Be(66);
+        }
+
+        [Test]
+        public void GetScore_AddsBonusScore_WhenSpareThenStrike()
+        {
+            game.Roll(2);
+            game.Roll(8);
+            game.Roll(10);
+            game.Roll(2);
+            game.GetScore().Should().Be(34);
+        }
+
+        [Test]
+        public void Roll_ThrowsGameEndedException_WhenMaxRollsReached()
+        {
+            Action action = () =>
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                    game.Roll(10);
+                }
+            };
+            action.ShouldThrow<IndexOutOfRangeException>();
+        }
+
+        [Test]
+        public void GetScore_ReturnsCorrectScore_When12Strikes()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Roll(10);
+            }
+            game.GetScore().Should().Be(300);
         }
     }
 }
