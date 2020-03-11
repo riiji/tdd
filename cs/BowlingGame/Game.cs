93f95f7b1d13f7b7ea94c167ed1934dc455fdc89
Ar-Chr <artyom.chr@mail.ru>
2020-03-11 11:03:57 +0300

    12

--- cs/BowlingGame/Game.cs
+++ cs/BowlingGame/Game.cs
@@ -9,8 +9,17 @@ namespace BowlingGame
     {
         private int _score = 0;
 
+        private int defaultPintsNumber = 10;
+        private int pintsLeft;
+        private bool firstStrike = true;
+
         public void Roll(int pins)
         {
+            if (pins < 0 || pins > 10)
+                throw new ArgumentException();
+
+            if
+
             _score += pins;
         }
 
@@ -23,10 +32,19 @@ namespace BowlingGame
     [TestFixture]
     public class Game_should : ReportingTest<Game_should>
     {
+
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
         [Test]
         public void HaveZeroScore_BeforeAnyRolls()
         {
-            new Game()
+            game
                 .GetScore()
                 .Should().Be(0);
         }
@@ -34,12 +52,39 @@ namespace BowlingGame
         [Test]
         public void HaveCorrectScore_AfterAnyRoll()
         {
-            var game = new Game();
             game.Roll(4);
             game.GetScore().Should().Be(4);
         }
 
+        [Test]
+        public void GetScore_ReturnsCorrectScore_AfterTwoRolls_WitnNoStrikes()
+        {
+            game.Roll(3);
+            game.Roll(4);
+            game.GetScore().Should().Be(7);
+        }
 
+        [Test]
+        public void Roll_DoesntAcceptNegativeNumbers()
+        {
+            Action action = () => game.Roll(-1);
+            action.ShouldThrow<ArgumentException>();
+        }
 
+        [Test]
+        public void Roll_DoesntAcceptNumbersMoreThatTen()
+        {
+            Action action = () => game.Roll(12);
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [Test]
+        public void GetScore_AddsBonusScpre_WhenSpare()
+        {
+            game.Roll(2);
+            game.Roll(8);
+            game.Roll(3);
+            game.GetScore().Should().Be(16);
+        }
     }
 }
