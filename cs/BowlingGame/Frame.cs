2d51ce9e76598ee74a6d6bb46a401168ef78b035
Riijii <riijii333@gmail.com>
2020-03-11 11:33:40 +0300

    frame system added

---
+++ cs/BowlingGame/Frame.cs
@@ -0,0 +1,11 @@
+﻿namespace BowlingGame
+{
+    public class Frame
+    {
+        public FrameState State { get; set; } = FrameState.None;
+        public int FirstThrow { get; set; }
+        public int SecondThrow { get; set; }
+        public int Bonus { get; set; }
+        public bool IsFirstThrow { get; set; } = true;
+    }
+}
